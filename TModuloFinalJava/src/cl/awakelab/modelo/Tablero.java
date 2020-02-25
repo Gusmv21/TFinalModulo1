d895b553f5765d064c8b44ee1297b8d45f4b2ef9
Aalumno <Aalumno@Note_21>
2020-02-25 20:01:06 -0300

    ppp

--- TModuloFinalJava/src/cl/awakelab/modelo/Tablero.java
+++ TModuloFinalJava/src/cl/awakelab/modelo/Tablero.java
@@ -2,20 +2,20 @@
  * 
  */
 package cl.awakelab.modelo;
-
+import java.util.*:
 /**
  * @author Gu
  *
  */
 public class Tablero {
 	
-	private int tablero;
-	private Carro carro[][];
+	private int tablero[][];
+	private Carro carro;
 	private ArrayList<Huevo> huevo;
 	static final int  DIM_DEFAUL_TABLERO = 15;
 
-	//constructor tablero
-	public Tablero( int[][] tablero, Carro carro, ArrayList<Huevo> huevo) {
+	//constructor tablero no se si esta correcto
+	public Tablero( int[][]tablero, Carro carro, ArrayList<Huevo> huevo) {
 		this.tablero = tablero[DIM_DEFAUL_TABLERO][DIM_DEFAUL_TABLERO];
 		huevo = new ArrayList<Huevo>();		
 		carro[][] = new Carro();	
