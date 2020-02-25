ce17c97e0544551f053033c95714c7684f1bdbde
Aalumno <Aalumno@Note_21>
2020-02-25 20:42:40 -0300

    nuevo

--- TModuloFinalJava/src/cl/awakelab/modelo/Carro.java
+++ TModuloFinalJava/src/cl/awakelab/modelo/Carro.java
@@ -2,68 +2,37 @@
  * 
  */
 package cl.awakelab.modelo;
-import java.util.Date;
-/**
- * @author Gonzalo Guzman/Gustavo Morales/ Paolo /Sebastian
- * Clase Padre Carro.
- */
 
+import java.util.*;
+
+/**
+ * @author Gonzalo Guzman/Gustavo Morales/ Paolo /Sebastian Clase Padre Carro.
+ */
 
 public class Carro {
 
 	private int cantOcupantes;
-	private Date fechaIngresoInstitucion;
-	private  String ubicacion; //aun no tengo claro como definir este atributo
+	private String fechaIngresoInstitucion;
+	private int ubicacionA;
+	private int ubicacionB;
 
-/**
- * @author Gonzalo Guzman/Gustavo Morales/ Paolo /Sebastian
- *constructor del objeto carro
- *@param cantOcupantes;
- *@param fechaIngresoInstitucion
- *@param ubicacion
- */
+	/**
+	 * @author Gonzalo Guzman/Gustavo Morales/ Paolo /Sebastian constructor del
+	 *         objeto carro
+	 * @param cantOcupantes;
+	 * @param fechaIngresoInstitucion
+	 * @param ubicacion
+	 */
 
 	public Carro(int cantOcupantes, Date fechaIngresoInstitucion, String ubicacion) {
 		this.cantOcupantes = cantOcupantes;
 		this.fechaIngresoInstitucion = fechaIngresoInstitucion;
-		this.ubicacion = ubicacionCarro();
+		this.ubicacionA = Math.random() * 15;
+		this.ubicacionB = Math.random() * 15;
 
+		
 	}
 
-	//metodo para definir ubicacion del objeto carro debe ser al azar
-
-	public  String  ubicacionCarro() {
-		return ubicacion;
-
-	}
-	//get de los tres atributos y set de fecha y cant ocupantes
-	
-	public int getCantOcupantes() {
-		return cantOcupantes;
-	}
-
-	public void setCantOcupantes(int cantOcupantes) {
-		this.cantOcupantes = cantOcupantes;
-	}
-
-	public Date getFechaIngresoInstitucion() {
-		return fechaIngresoInstitucion;
-	}
-
-	public void setFechaIngresoInstitucion(Date fechaIngresoInstitucion) {
-		this.fechaIngresoInstitucion = fechaIngresoInstitucion;
-	}
-
-	public String getUbicacion() {
-		return ubicacion;
-	}
-//to string clase carro
-	@Override
-	public String toString() {
-		return "Carro [cantOcupantes=" + cantOcupantes + ", fechaIngresoInstitucion=" + fechaIngresoInstitucion
-				+ ", ubicacion=" + ubicacion + "]";
-	}
 	
 	
-
 }
