d996efac35cefc03bb8ce65458e22dc046f99c36
Aalumno <Aalumno@Note_21>
2020-02-25 20:58:32 -0300

    Clase carro lista

--- TModuloFinalJava/src/cl/awakelab/modelo/Carro.java
+++ TModuloFinalJava/src/cl/awakelab/modelo/Carro.java
@@ -1,12 +1,13 @@
 /**
- * 
+ * Clase Carro Proyecto Modulo Java
  */
 package cl.awakelab.modelo;
 
 import java.util.*;
 
 /**
- * @author Gonzalo Guzman/Gustavo Morales/ Paolo /Sebastian Clase Padre Carro.
+ * @author Gonzalo Guzman/Gustavo Morales/ Paolo Mancini /Sebastian Ramirez 
+ * Clase Padre Carro.
  */
 
 public class Carro {
@@ -17,11 +18,12 @@ public class Carro {
 	private int ubicacionB;
 
 	/**
-	 * @author Gonzalo Guzman/Gustavo Morales/ Paolo /Sebastian constructor del
-	 *         objeto carro
+	 * @author Gonzalo Guzman/Gustavo Morales/ Paolo Mancini /Sebastian Ramirez
+	 *  Constructor del objeto Carro
 	 * @param cantOcupantes;
 	 * @param fechaIngresoInstitucion
-	 * @param ubicacion
+	 * @param ubicacionA;
+	 * @param ubicacionB;
 	 */
 
 	public Carro(int cantOcupantes, Date fechaIngresoInstitucion, String ubicacion) {
@@ -33,6 +35,49 @@ public class Carro {
 		
 	}
 
+	//get y set de los atributos
+
+	public int getCantOcupantes() {
+		return cantOcupantes;
+	}
+
+	public void setCantOcupantes(int cantOcupantes) {
+		this.cantOcupantes = cantOcupantes;
+	}
+
+	public String getFechaIngresoInstitucion() {
+		return fechaIngresoInstitucion;
+	}
+
+	public void setFechaIngresoInstitucion(String fechaIngresoInstitucion) {
+		this.fechaIngresoInstitucion = fechaIngresoInstitucion;
+	}
+	public int getUbicacionA() {
+		return ubicacionA;
+	}
+
+	private void setUbicacionA(int ubicacionA) {
+		this.ubicacionA = ubicacionA;
+	}
 	
+	public int getUbicacionB() {
+		return ubicacionB;
+	}
+
+	private void setUbicacionB(int ubicacionB) {
+		this.ubicacionB = ubicacionB;
+	}
+	
+	// to string clase Carro
+
+	@Override
+	public String toString() {
+		return "Carro [Cantidad Ocupantes: " + cantOcupantes + ", Fecha Ingreso Institucion: " + fechaIngresoInstitucion 
+				+ ", ubicacion: ("+ubicacionA+","+ubicacionB+")";
+	}
+	
+	
+}
+
 	
 }
